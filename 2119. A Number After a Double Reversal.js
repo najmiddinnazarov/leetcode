7c145e69090156b687386fec86aecf70a9e50735
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-06-18 16:30:40 +0500

    [200~2119. A Number After a Double Reversal

---
+++ 2119. A Number After a Double Reversal.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isSameAfterReversals = function (num) {
+  const reversed1 = +[...`${num}`].reverse().join("") * 1;
+  const reversed2 = +[...`${reversed1}`].reverse().join("");
+  return reversed2 === num;
+};
+console.log(isSameAfterReversals(526));
